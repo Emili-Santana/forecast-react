4da7ba07cdd7ac04f607e939276d12efcff239a1
Seu Nome <santanaemili@gmail.com>
2024-12-20 16:56:58 +0000

    Updated the component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
@@ -8,29 +9,27 @@ export default function WeatherInfo(props) {
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
       <div className="row">
-        <div className="col-6">
-          <div className="left">
-            <WeatherIcon code={props.data.icon} size={52} />
-            <WeatherTemperature celsius={props.data.temperature} />
-            <div className="detail mt-1">
-              <ul className="ps-4">
-                <li>Precipitation: 0%</li>
-                <li>Humidity: {props.data.humidity}%</li>
-                <li>Wind: {props.data.wind} km/h</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul className="short-description">
-            <li>
-              <h3>Weather</h3>
-            </li>
+        <div className="col-12 col-md-6">
+          <ul className="description">
             <li>
               <FormattedDate date={props.data.date} />
             </li>
+            
             <li className="text-capitalize">{props.data.description}</li>
           </ul>
+        </div>
+
+        <div className="col-12 col-md-6 d-flex flex-column align-items-center">
+          <div className="left">
+            <WeatherIcon code={props.data.icon} size={52} />
+            <WeatherTemperature celsius={props.data.temperature} />
+          </div>
+          <div className="detail-weather mt-3">
+            <ul>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {props.data.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
+const [unit, setUnit] = useState("celsius")
+
+function showFahrenheit (event){
+event.preventDefault();
+setUnit("fahrenheit");
+}
+
+function showCelsius (event){
+    event.preventDefault();
+    setUnit("celsius");
+    }
+if (unit === "celsius"){
   return (
-    <div className="WeatherTemperture">
+    <div className="WeatherTemperature">
       <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">
-        {" "}
-        °C
+      <span className="unit">°C | {" "}
+        <a href="/" onClick={showFahrenheit}>
+        °F
+        </a>
       </span>
     </div>
   );
+} else {
+    let fahrenheit = (props.celsius*9/5) + 32; //Convert to fahrenheit, but I can create a function for it
+return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(fahrenheit)}</span>
+      <span className="unit">°F| {" "}
+        <a href="/" onClick={showCelsius}>
+        °C
+        </a>
+      </span>
+    </div>
+);
+
+}
+
 }
