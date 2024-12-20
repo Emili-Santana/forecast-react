b46a3f50d9de42b4992b13a8feb932559df14766
Seu Nome <santanaemili@gmail.com>
2024-12-20 17:29:02 +0000

    Added some icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,13 +1,16 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-
+import "bootstrap-icons/font/bootstrap-icons.css";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
+        <h1 className="mb-3">
+             <i className="bi bi-geo-alt-fill"> </i>
+                {props.data.city}
+             </h1>
       <div className="row">
         <div className="col-12 col-md-6">
           <ul className="description">
@@ -25,10 +28,14 @@ export default function WeatherInfo(props) {
             <WeatherTemperature celsius={props.data.temperature} />
           </div>
           <div className="detail-weather mt-3">
-            <ul>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind} km/h</li>
-            </ul>
+            <ul>  
+                            <li className="mt-4">
+                            <i class="bi bi-droplet"> </i>
+                                Humidity: {props.data.humidity}%</li>
+                            <li>
+                            <i className="bi bi-wind"> </i>
+                                Wind: {props.data.wind} m/s</li>
+                        </ul>
           </div>
         </div>
       </div>
